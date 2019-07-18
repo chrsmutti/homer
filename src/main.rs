227b312787bfa3e0bad6e30aee2a1cee1be0a2be
Christian Mutti <chrsmutti@gmail.com>
2019-07-17 21:54:07 -0300

    Added some error handling and better documentation

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() -> io::Result<()> {
     let dir = home.map(|e| fs::read_dir(e.path())).unwrap()?;
     dir.filter_map(|f| f.ok()).for_each(|f| {
         match process_entry(&f, &dirs::home_dir().expect("Failed to get $HOME")) {
-            Err(e) => println!("Error while processing {:?}: {}", f.file_name(), e),
+            Err(e) => eprintln!("Error while processing {:?}: {}", f.file_name(), e),
             _ => {}
         }
     });
@@ -26,7 +26,7 @@ fn main() -> io::Result<()> {
 }
 
 /// Process a generic entry, could be a dir or a file.
-fn process_entry(entry: &fs::DirEntry, parent_path: &Path) -> Result<(), io::Error> {
+fn process_entry(entry: &fs::DirEntry, parent_path: &Path) -> io::Result<()> {
     if entry.path().is_dir() {
         process_dir(entry, parent_path)
     } else {
@@ -37,6 +37,12 @@ fn process_entry(entry: &fs::DirEntry, parent_path: &Path) -> Result<(), io::Err
 /// Process a dir entry in home directory.
 ///
 /// The files inside the entry directory will be processed.
+/// # Errors
+///
+/// This function will return an error in the following situations, but is not
+/// limited to just these cases:
+///
+/// * Regular file exists at target location.
 fn process_dir(entry: &fs::DirEntry, parent_path: &Path) -> io::Result<()> {
     let mut buf = PathBuf::new();
     buf.push(parent_path);
@@ -53,7 +59,15 @@ fn process_dir(entry: &fs::DirEntry, parent_path: &Path) -> io::Result<()> {
             fs::create_dir(&buf)?
         }
         Err(e) => return Err(e),
-        _ => {}
+        Ok(stat) => {
+            if !stat.is_dir() {
+                eprintln!(
+                    "A regular file exists at {:?}, couldn't create a directory",
+                    buf
+                );
+                return Err(io::Error::from(io::ErrorKind::AlreadyExists));
+            }
+        }
     }
 
     let dir = fs::read_dir(entry.path())?;
@@ -68,13 +82,57 @@ fn process_dir(entry: &fs::DirEntry, parent_path: &Path) -> io::Result<()> {
 
 /// Process a file entry in home directory.
 ///
-/// The file will be linked into `$HOME/{{ parent_path }}/{{ entry }}`
+/// The file will be linked into `$HOME/{{ parent_path }}/{{ entry }}`.
+/// It checks if a regular file exists at the location, or if a symlink already
+/// exists.
+///
+/// # Errors
+///
+/// This function will return an error in the following situations, but is not
+/// limited to just these cases:
+///
+/// * Same symlink or regular file already exists and no `force` option was passed.
+/// * Different symlink exists at path and no `overwrite-symlinks` option was passed.
 fn process_file(entry: &fs::DirEntry, parent_path: &Path) -> io::Result<()> {
     let mut buf = PathBuf::new();
     buf.push(parent_path);
     buf.push(Path::new(
         entry.path().file_name().expect("Failed to get file name"),
     ));
+
+    // Check for already created symlinkins to avoid doing it again, and also
+    // check if the files already exist in some way.
+    match fs::symlink_metadata(&buf) {
+        Ok(stat) => {
+            if stat.file_type().is_symlink() {
+                let cpath = fs::canonicalize(&buf)?;
+                if cpath == entry.path() {
+                    eprintln!("Symlink {:?} already points to {:?}", buf, entry.path());
+                    // TODO: Add a -f (--force) option.
+                    return Ok(());
+                } else {
+                    eprintln!("Symlink {:?} points to another path {:?}", buf, cpath);
+                    // TODO: Add an --ovewrite-symlinks option.
+                    return Err(io::Error::from(io::ErrorKind::PermissionDenied));
+                }
+            } else {
+                eprintln!(
+                    concat!(
+                        "Regular file {:?} already exists and it's not a symlink.\n",
+                        "Use --backup to create a backup of this file before symlinking it to {:?}, ",
+                        "or --force to delete the file without backup."
+                    ),
+                    buf,
+                    entry.path()
+                );
+
+                // TODO: Add a --backup option and a --force option.
+                return Err(io::Error::from(io::ErrorKind::PermissionDenied));
+            }
+        }
+        Err(ref e) if e.kind() == io::ErrorKind::NotFound => {}
+        Err(e) => return Err(e),
+    }
 
     println!(
         "Linking {:?} to destination: {:?}",
