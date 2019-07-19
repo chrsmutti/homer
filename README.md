a013fe8a4aee43b9353737a2cd897605f548c991
Christian Mutti <chrsmutti@gmail.com>
2019-07-19 00:23:08 -0300

    Finish adding all flags, and some minor API improvements

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # homer
 
-A dotfiles manager.
+"Doh!" A CLI for managing your dotfiles!
+
+## Usage
+
+You can run `homer` from a directory containing a `home` directory, or pass in
+an input directory.
+
+- All files from the input directory will be linked into `$HOME`.
+- All directories from the input directory will be created into `$HOME`.
+
+```
+homer 0.1.0
+Christian Mutti <chrsmutti@gmail.com>
+"Doh!" A CLI for managing your dotfiles!
+
+USAGE:
+    homer [FLAGS] [input]
+
+FLAGS:
+    -b, --backup                If a regular file is found at a location that a symlink should be created, the file will
+                                be backed up to a file with the same name, with a .bkp extension. Any old backup file
+                                will be overwritten.
+    -f, --force                 Force symlink creation even if a regular file exists at the location (deletes the old
+                                file).
+    -h, --help                  Prints help information
+        --overwrite-symlinks    If a symlink to another path exists, overwrite it.
+    -V, --version               Prints version information
+    -v, --verbose               Show verbose output about the operations.
+
+ARGS:
+    <input>    Directory containing files to link into user's home directory. (Defaults to ./home)
+```
+
+### This could be a bash script!!1!
+
+Yes.
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,60 @@
 use dirs;
+use structopt;
+
 use std::path::{Path, PathBuf};
 use std::{env, fs, io, os::unix};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+/// "Doh!" A CLI for managing your dotfiles!
+struct Opt {
+    #[structopt(short = "f", long = "force")]
+    /// Force symlink creation even if a regular file exists at the location (deletes the old file).
+    force: bool,
+
+    #[structopt(short = "b", long = "backup")]
+    /// If a regular file is found at a location that a symlink should be created, the file will
+    /// be backed up to a file with the same name, with a .bkp extension. Any old backup file will
+    /// be overwritten.
+    backup: bool,
+
+    #[structopt(long = "overwrite-symlinks")]
+    /// If a symlink to another path exists, overwrite it.
+    overwrite_symlinks: bool,
+
+    #[structopt(short = "v", long = "verbose")]
+    /// Show verbose output about the operations.
+    verbose: bool,
+
+    /// Directory containing files to link into user's home directory. (Defaults to ./home)
+    #[structopt(parse(from_os_str))]
+    input: Option<PathBuf>,
+}
 
 fn main() -> io::Result<()> {
-    println!("Welcome to homer.");
-    let dir = fs::read_dir(env::current_dir()?)?;
+    let opt = Opt::from_args();
 
-    let home = dir
-        .filter_map(|f| f.ok())
-        .find(|f| f.file_name() == "home" && f.path().is_dir());
-    if home.is_none() {
-        eprintln!("No 'home' dir found in current directory.");
-        return Ok(());
-    }
+    let input = opt
+        .input
+        .clone()
+        .ok_or(io::Error::from(io::ErrorKind::InvalidInput))
+        .or::<io::Error>({
+            let dir = fs::read_dir(env::current_dir()?)?;
 
-    let dir = home.map(|e| fs::read_dir(e.path())).unwrap()?;
-    dir.filter_map(|f| f.ok()).for_each(|f| {
-        match process_entry(&f, &dirs::home_dir().expect("Failed to get $HOME")) {
+            let home = dir
+                .filter_map(|f| f.ok())
+                .find(|f| f.file_name() == "home" && f.path().is_dir())
+                .expect("No 'home' dir found in current directory.");
+
+            Ok(home.path())
+        })?;
+
+    let input = fs::canonicalize(input)?;
+    verbose!(opt.verbose, "Running homer using files from: {:?}.", input);
+    let entries = fs::read_dir(input)?;
+
+    entries.filter_map(|f| f.ok()).for_each(|f| {
+        match process_entry(&f, &dirs::home_dir().expect("Failed to get $HOME"), &opt) {
             Err(e) => eprintln!("Error while processing {:?}: {}", f.file_name(), e),
             _ => {}
         }
@@ -26,35 +64,36 @@ fn main() -> io::Result<()> {
 }
 
 /// Process a generic entry, could be a dir or a file.
-fn process_entry(entry: &fs::DirEntry, parent_path: &Path) -> io::Result<()> {
+fn process_entry(entry: &fs::DirEntry, parent_path: &Path, opt: &Opt) -> io::Result<()> {
     if entry.path().is_dir() {
-        process_dir(entry, parent_path)
+        process_dir(entry, parent_path, opt)
     } else {
-        process_file(entry, parent_path)
+        process_file(entry, parent_path, opt)
     }
 }
 
 /// Process a dir entry in home directory.
 ///
 /// The files inside the entry directory will be processed.
+///
 /// # Errors
 ///
 /// This function will return an error in the following situations, but is not
 /// limited to just these cases:
 ///
 /// * Regular file exists at target location.
-fn process_dir(entry: &fs::DirEntry, parent_path: &Path) -> io::Result<()> {
+fn process_dir(entry: &fs::DirEntry, parent_path: &Path, opt: &Opt) -> io::Result<()> {
     let mut buf = PathBuf::new();
     buf.push(parent_path);
     buf.push(Path::new(
         entry.path().file_name().expect("Failed to get file name."),
     ));
 
-    println!("Checking if {:?} exists.", buf);
+    verbose!(opt.verbose, "Checking if {:?} exists.", buf);
 
     match fs::metadata(&buf) {
         Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
-            println!("Creating {:?}", buf);
+            verbose!(opt.verbose, "Creating directory at {:?}", buf);
 
             fs::create_dir(&buf)?
         }
@@ -72,7 +111,7 @@ fn process_dir(entry: &fs::DirEntry, parent_path: &Path) -> io::Result<()> {
 
     let dir = fs::read_dir(entry.path())?;
     dir.filter_map(|f| f.ok())
-        .for_each(|f| match process_entry(&f, &buf) {
+        .for_each(|f| match process_entry(&f, &buf, opt) {
             Err(e) => println!("Error while processing {:?}: {}", f.file_name(), e),
             _ => {}
         });
@@ -93,7 +132,7 @@ fn process_dir(entry: &fs::DirEntry, parent_path: &Path) -> io::Result<()> {
 ///
 /// * Same symlink or regular file already exists and no `force` option was passed.
 /// * Different symlink exists at path and no `overwrite-symlinks` option was passed.
-fn process_file(entry: &fs::DirEntry, parent_path: &Path) -> io::Result<()> {
+fn process_file(entry: &fs::DirEntry, parent_path: &Path, opt: &Opt) -> io::Result<()> {
     let mut buf = PathBuf::new();
     buf.push(parent_path);
     buf.push(Path::new(
@@ -107,34 +146,72 @@ fn process_file(entry: &fs::DirEntry, parent_path: &Path) -> io::Result<()> {
             if stat.file_type().is_symlink() {
                 let cpath = fs::canonicalize(&buf)?;
                 if cpath == entry.path() {
-                    eprintln!("Symlink {:?} already points to {:?}", buf, entry.path());
-                    // TODO: Add a -f (--force) option.
-                    return Ok(());
+                    if !opt.force {
+                        eprintln!(
+                            concat!(
+                                "Symlink {:?} already points to {:?}.\n",
+                                "Use --force to do the operation anyway."
+                            ),
+                            buf,
+                            entry.path()
+                        );
+                        // If no force is specified, do nothing.
+                        return Err(io::Error::from(io::ErrorKind::AlreadyExists));
+                    }
+
+                    println!(
+                        "Removing old symlink at {:?}, due to --force (-f) flag",
+                        &buf
+                    );
+                    // Remove old symlink
+                    fs::remove_file(&buf)?
                 } else {
-                    eprintln!("Symlink {:?} points to another path {:?}", buf, cpath);
-                    // TODO: Add an --ovewrite-symlinks option.
-                    return Err(io::Error::from(io::ErrorKind::PermissionDenied));
+                    if !opt.overwrite_symlinks {
+                        eprintln!(
+                            concat!(
+                                "Symlink {:?} points to another path {:?}.\n",
+                                "Use --overwrite-symlinks to do the operation anyway."
+                            ),
+                            buf, cpath
+                        );
+                        return Err(io::Error::from(io::ErrorKind::PermissionDenied));
+                    }
+
+                    println!(
+                        "Removing old symlink at {:?}, due to --overwrite-symlinks flag",
+                        &buf
+                    );
+                    // Remove old symlink
+                    fs::remove_file(&buf)?
                 }
             } else {
-                eprintln!(
-                    concat!(
-                        "Regular file {:?} already exists and it's not a symlink.\n",
-                        "Use --backup to create a backup of this file before symlinking it to {:?}, ",
-                        "or --force to delete the file without backup."
-                    ),
-                    buf,
-                    entry.path()
-                );
+                if !opt.backup {
+                    eprintln!(
+                        concat!(
+                            "Regular file {:?} already exists and it's not a symlink.\n",
+                            "Use --backup to create a backup of this file before symlinking it to {:?}, ",
+                            "or --force to delete the file without backup."
+                        ),
+                        buf,
+                        entry.path()
+                    );
+                    return Err(io::Error::from(io::ErrorKind::PermissionDenied));
+                }
 
-                // TODO: Add a --backup option and a --force option.
-                return Err(io::Error::from(io::ErrorKind::PermissionDenied));
+                let bpath = buf.with_extension("bkp");
+                println!(
+                    "Backing up {:?} to {:?} due to --backup (-b) flag.",
+                    buf, bpath
+                );
+                fs::rename(&buf, bpath)?;
             }
         }
         Err(ref e) if e.kind() == io::ErrorKind::NotFound => {}
         Err(e) => return Err(e),
     }
 
-    println!(
+    verbose!(
+        opt.verbose,
         "Linking {:?} to destination: {:?}",
         entry.path().as_path(),
         buf
@@ -142,4 +219,13 @@ fn process_file(entry: &fs::DirEntry, parent_path: &Path) -> io::Result<()> {
     unix::fs::symlink(entry.path(), buf)?;
 
     Ok(())
+}
+
+#[macro_export]
+macro_rules! verbose {
+    ($should:expr, $($arg:tt)*) => {{
+        if $should {
+            println!($($arg)*);
+        }
+    }};
 }
